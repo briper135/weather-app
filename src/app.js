a465f15b7a36cdd0bc985b167947cfcd26aa58f0
briper135 <briper135@gmail.com>
2021-09-05 22:53:45 +0200

    Added icon from API and removed pictures

--- src/app.js
+++ src/app.js
@@ -22,17 +22,22 @@ function displayTemperature(response){
     let humidityElement = document.querySelector("#humidity");
     let windElement = document.querySelector("#wind");
     let dateElement = document.querySelector("#date");
+    let iconElement = document.querySelector("#icon");
+
     temperatureElement.innerHTML = Math.round(response.data.main.temp);
     cityElement.innerHTML = response.data.name;
     descriptionElement.innerHTML = response.data.weather[0].description;
     humidityElement.innerHTML = response.data.main.humidity;
     windElement.innerHTML = Math.round(response.data.wind.speed);
     dateElement.innerHTML = formatDate(response.data.dt*1000);
+    iconElement.setAttribute("src", `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`); 
+    iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
 
 
 let apiKey = "d1bbd379750cfbcbbe5aa91d99dcdec2";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=london,uk&appid=${apiKey}&units=metric`;
+let city = "London,uk";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
 axios.get(apiUrl).then(displayTemperature);
