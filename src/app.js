81bceb899742224479f1ed6ad7f1444c36adc75d
briper135 <briper135@gmail.com>
2021-09-05 23:23:33 +0200

    Added search engine

--- src/app.js
+++ src/app.js
@@ -34,10 +34,20 @@ function displayTemperature(response){
     iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
-
-
+function search(city) {
 let apiKey = "d1bbd379750cfbcbbe5aa91d99dcdec2";
-let city = "London,uk";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
 axios.get(apiUrl).then(displayTemperature);
+}
+function handleSubmit(event) {
+    event.preventDefault();
+    let cityInputElement = document.querySelector("#city-input");
+    search(cityInputElement.value);
+}
+
+
+search("New York");
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
